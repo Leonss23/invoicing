6813628a98c32b4225e6aa4eaf237278322aff7a
Leonss23 <leonardogattidev@gmail.com>
2023-10-04 15:20:36 -0300

    completed first lesson

---
+++ src/main/java/com/yourcompany/invoicing/model/Product.java
@@ -0,0 +1,23 @@
+package com.yourcompany.invoicing.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.openxava.annotations.Required;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @Column(length = 9)
+    int number;
+
+    @Column(length = 50)
+    @Required
+    String description;
+}
